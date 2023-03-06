c889eafc603f135c808ede476435f2c5eb6f0b8b
rishiraj007 <rishiraj@mounttalent.com>
2023-03-06 16:26:11 +0530

    [Prabhu] Add: Linked List Created

---
+++ LinkedList-Stack-And-Queue/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Stack_And_Queue;
+
+public class Node
+{
+    public int data;
+    public Node next;
+    public Node(int data)
+    {
+        this.data = data;
+    }
+}
