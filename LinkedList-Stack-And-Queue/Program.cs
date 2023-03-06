cc6006f33ac0f742f0111ad65d902c4d846a2177
rishiraj007 <rishiraj@mounttalent.com>
2023-03-06 16:21:12 +0530

    [Prabhu] Add: Linked List Stack and Queue

---
+++ LinkedList-Stack-And-Queue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedList_Stack_And_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LinkedList-Stack-and-Queue!");
+        }
+    }
+}
